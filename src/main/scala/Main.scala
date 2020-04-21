1957b336254fbaf2169d6267fee995b259a92e93
Arnout Engelen <arnout@bzzt.net>
2020-04-21 18:46:14 +0200

    Add 'setup', minify

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,13 @@
 import scala.concurrent.duration._
 
 import akka.actor.typed._
+import akka.actor.typed.scaladsl._
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
+
+import SessionManager._
 
 object SessionManager {
   sealed trait Command
@@ -12,51 +15,37 @@ object SessionManager {
 
   case class LoginAttempt(descriptor: String)
 
-  def apply(): Behavior[Command] = Behaviors.receiveMessage {
+  def apply(): Behavior[Command] = Behaviors.setup(new SessionManager(_))
+}
+
+class SessionManager(context: ActorContext[Command]) extends AbstractBehavior[Command](context) {
+
+  override def onMessage(msg: Command): Behavior[Command] = Behaviors.receiveMessage {
     case Login(session, replyTo) =>
       replyTo ! LoginAttempt(s"Hi $session!")
       Behaviors.same
   }
 }
 
-object EngineExtension extends ExtensionId[EngineExtension] {
-
-  override def createExtension(system: ActorSystem[_]): EngineExtension = new EngineExtension(system)
-}
-
-class EngineExtension(system: ActorSystem[_]) extends Extension {
-  val sessionManager: ActorRef[SessionManager.Command] = {
-    val ref = system.toClassic.spawn(SessionManager(), name = "session-manager")
-    //ref ! Ping // Remove this and the initial ask always fails
-    ref
-  }
-}
-
-
-object ClassicEngineExtension extends akka.actor.ExtensionId[ClassicEngineExtension] with akka.actor.ExtensionIdProvider {
-
-  override def createExtension(system: akka.actor.ExtendedActorSystem): ClassicEngineExtension = new ClassicEngineExtension(system)
-
-  override def lookup(): akka.actor.ExtensionId[_ <: akka.actor.Extension] = ClassicEngineExtension
-}
-
-class ClassicEngineExtension(system: akka.actor.ActorSystem) extends akka.actor.Extension {
-  val engineExtension: EngineExtension = new EngineExtension(system.toTyped)
-}
-
 object Main extends App {
-  val system = akka.actor.ActorSystem("reproducer")
+  val system: ActorSystem[Command] = 
+    ActorSystem(SessionManager(), "reproducer")
+
+  // This is printed:
+  system.log.info("Started")
+
+  val sessionManager: ActorRef[Command] = system
 
   implicit val timeout = Timeout(1.second)
-  implicit val scheduler = system.toTyped.scheduler
-  implicit val ec = system.dispatcher
+  implicit val scheduler = system.scheduler
+  implicit val ec = system.toClassic.dispatcher
 
-    val engineExtension = ClassicEngineExtension(system).engineExtension
-
-    import SessionManager._
-    val session = "Foo"
-    engineExtension.sessionManager.ask[LoginAttempt](Login(session, _)).foreach { response =>
-      system.log.info(s"Got response $response")
-      system.terminate()
-    }
+  sessionManager.ask[LoginAttempt](Login("Foo", _)).foreach { response =>
+    // This is not printed:
+    system.log.info(s"Got response $response")
+  }
+  sessionManager.ask[LoginAttempt](Login("Bar", _)).foreach { response =>
+    // This is printed:
+    system.log.info(s"Got response $response")
+  }
 }
